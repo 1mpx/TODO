d4d7e629f97cb911bffaa0b1bbdeefe07b89b6ab
1mpx <serega.semen2011@yandex.ru>
2024-06-10 21:42:31 +0300

    first commit

---
+++ web/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z db 5432; do
+      sleep 0.1
+    done
+
+    echo "Postgres started"
+fi
+
+python manage.py flush --no-input
+python manage.py migrate
+
+if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] && [ -n "$DJANGO_SUPERUSER_EMAIL" ]; then
+    python manage.py createsuperuser --noinput || true
+fi
+
+python manage.py collectstatic --noinput
+
+exec "$@"
+
